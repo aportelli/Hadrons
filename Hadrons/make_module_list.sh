ba973d21087a12aad0a243ac34f64b79589daddd
Nils Asmussen <n.asmussen@soton.ac.uk>
2020-04-01 18:52:14 +0100

    make sorting of modules independent of locale

--- Hadrons/make_module_list.sh
+++ Hadrons/make_module_list.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 echo 'modules_cpp =\' > modules.inc
-find Modules -name '*.cpp' -type f -print | sort | sed 's/^/  /;$q;s/$/ \\/' >> modules.inc
+find Modules -name '*.cpp' -type f -print | LC_ALL=C sort | sed 's/^/  /;$q;s/$/ \\/' >> modules.inc
 echo '' >> modules.inc
 echo 'modules_hpp =\' >> modules.inc
-find Modules -name '*.hpp' -type f -print | sort | sed 's/^/  /;$q;s/$/ \\/' >> modules.inc
+find Modules -name '*.hpp' -type f -print | LC_ALL=C sort | sed 's/^/  /;$q;s/$/ \\/' >> modules.inc
 echo '' >> modules.inc
 rm -f Modules.hpp
-for f in $(find Modules -name '*.hpp' | sort); do
+for f in $(find Modules -name '*.hpp' | LC_ALL=C sort); do
 	echo "#include <Hadrons/${f}>" >> Modules.hpp
 done
