eb0e10c4d72539a170a03be589c0239216c122bd
Antonin Portelli <antonin.portelli@me.com>
2020-04-16 18:12:36 +0100

    embedding SQLite code

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,9 @@ set -e
 echo '-- generating configure script...'
 mkdir -p .buildutils/m4
 autoreconf -fvi
+echo '-- downloading SQLite...'
+wget https://www.sqlite.org/2020/sqlite-amalgamation-3310100.zip
+unzip sqlite-amalgamation-3310100.zip
+mkdir -p Hadrons/sqlite/
+mv sqlite-amalgamation-3310100/sqlite3* Hadrons/sqlite/
+rm -rf sqlite-amalgamation-3310100 sqlite-amalgamation-3310100.zip
