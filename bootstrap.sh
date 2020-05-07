29bb72bc07140b99a54532483321bf433ce614cf
Antonin Portelli <antonin.portelli@me.com>
2020-05-07 11:38:27 +0100

    Modules.hpp and modules.inc now generated by bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+echo '-- generating module list...'
+cd Hadrons
+./make_module_list.sh
+cd ..
 echo '-- generating configure script...'
 mkdir -p .buildutils/m4
 autoreconf -fvi
