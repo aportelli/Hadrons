4c760ae4765109797d7ed27ee458f919466af420
Antonin Portelli <antonin.portelli@me.com>
2021-11-22 18:57:14 +0000

    test for high frequency stats

---
+++ tests/Test_highfreq_stat.cpp
@@ -0,0 +1,49 @@
+/*
+ * Test_highfreq_stat.cpp, part of Hadrons (https://github.com/aportelli/Hadrons)
+ *
+ * Copyright (C) 2015 - 2021
+ *
+ * Author: Antonin Portelli <antonin.portelli@me.com>
+ *
+ * Hadrons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hadrons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hadrons.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See the full license in the file "LICENSE" in the top level distribution 
+ * directory.
+ */
+
+/*  END LEGAL */
+
+#include <Hadrons/Database.hpp>
+#include <Hadrons/StatLogger.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+
+int main(int argc, char *argv[])
+{
+    Grid_init(&argc, &argv);
+
+    // high-frequency stat
+    Database   db("test-highfreq-stat.db");    
+    StatLogger stat(db, 1);
+
+    stat.start();
+    // do nothing
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    stat.stop();
+
+    Grid_finalize();
+    
+    return EXIT_SUCCESS;
+}
