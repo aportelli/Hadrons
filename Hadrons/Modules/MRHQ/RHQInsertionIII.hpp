2e1b39095a24b3b175495fe95f7e120152e8c40b
Alessandro Barone <ab1n19@soton.ac.uk>
2022-02-13 14:07:45 +0000

    Add flag and LefTRight implementation

--- Hadrons/Modules/MRHQ/RHQInsertionIII.hpp
+++ Hadrons/Modules/MRHQ/RHQInsertionIII.hpp
@@ -35,8 +35,10 @@
 BEGIN_HADRONS_NAMESPACE
 
 /******************************************************************************
- *                                 RHQInsertionIII                                      *
+ *                             RHQInsertionIII                                *
  ******************************************************************************/
+GRID_SERIALIZABLE_ENUM(OpFlagIII, undef, Chroma, 0, LeftRight, 1);
+
 BEGIN_MODULE_NAMESPACE(MRHQ)
 
 class RHQInsertionIIIPar: Serializable
@@ -44,7 +46,10 @@ class RHQInsertionIIIPar: Serializable
 public:
     GRID_SERIALIZABLE_CLASS_MEMBERS(RHQInsertionIIIPar,
                                     std::string, q,
-                                    std::string, gauge);
+                                    unsigned int,   index,
+                                    Gamma::Algebra, gamma5,
+                                    std::string,    gauge,
+                                    OpFlagIII,      flag);
 };
 
 template <typename FImpl, typename GImpl>
@@ -53,7 +58,7 @@ class TRHQInsertionIII: public Module<RHQInsertionIIIPar>
 public:
     BASIC_TYPE_ALIASES(FImpl,);
     GAUGE_TYPE_ALIASES(GImpl,)
-    SINK_TYPE_ALIASES();
+    SINK_TYPE_ALIASES(); // do we need this?
 public:
     // constructor
     TRHQInsertionIII(const std::string name);
@@ -108,25 +113,87 @@ void TRHQInsertionIII<FImpl, GImpl>::setup(void)
 template <typename FImpl, typename GImpl>
 void TRHQInsertionIII<FImpl, GImpl>::execute(void)
 {
-    LOG(Message) << "Applying Improvement term III to'" << par().q
+    LOG(Message) << "Applying Improvement term III with index " << par().index
+                 << " and gamma5=" << par().gamma5 
+                 << " to '" << par().q 
+                 << "' with flag '" << par().flag << "'"
                  << std::endl;
-
-    auto &field    = envGet(PropagatorField, par().q);
+    
+    Gamma g5(par().gamma5); // should we check that it's really either Gamma5 or Identity? Use enum here as well?
+    
+    auto &field = envGet(PropagatorField, par().q);
     const auto &gaugefield  = envGet(GaugeField, par().gauge);
     const auto gauge_x = peekLorentz(gaugefield, 0);
     const auto gauge_y = peekLorentz(gaugefield, 1);
     const auto gauge_z = peekLorentz(gaugefield, 2);
 
-    Gamma gx(Gamma::Algebra::GammaX);
-    Gamma gy(Gamma::Algebra::GammaY);
-    Gamma gz(Gamma::Algebra::GammaZ);
-    PropagatorField  insertion = 
-        gx * (GImpl::CovShiftForward( gauge_x,0,field) - GImpl::CovShiftBackward( gauge_x,0,field))
-     + gy * (GImpl::CovShiftForward( gauge_y,1,field) - GImpl::CovShiftBackward( gauge_y,1,field))
-     + gz * (GImpl::CovShiftForward( gauge_z,2,field) - GImpl::CovShiftBackward( gauge_z,2,field));
-     
-    auto &out  = envGet(PropagatorField, getName());
-    out = insertion;
+    auto &out = envGet(PropagatorField, getName());
+    if (par().flag == OpIIIFlag::Chroma)
+    {   
+        Gamma gx(Gamma::Algebra::GammaX);
+        Gamma gy(Gamma::Algebra::GammaY);
+        Gamma gz(Gamma::Algebra::GammaZ);
+
+        Gamma::Algebra gi; 
+        switch(par().index){
+            case 0:
+                gi = Gamma::Algebra::GammaX;
+                break;
+            case 1:
+                gi = Gamma::Algebra::GammaY;
+                break;
+            case 2:
+                gi = Gamma::Algebra::GammaZ;
+                break;
+            case 3:
+                gi = Gamma::Algebra::GammaT;
+                break;
+            default:
+                HADRONS_ERROR(Argument, "Index must be in {0, 1, 2, 3}."); 
+        }
+        PropagatorField insertion = 
+            gi*g5*gx * (GImpl::CovShiftForward(gauge_x,0,field) - GImpl::CovShiftBackward(gauge_x,0,field))
+          + gi*g5*gy * (GImpl::CovShiftForward(gauge_y,1,field) - GImpl::CovShiftBackward(gauge_y,1,field))
+          + gi*g5*gz * (GImpl::CovShiftForward(gauge_z,2,field) - GImpl::CovShiftBackward(gauge_z,2,field));
+        
+        out = insertion;
+    }
+    else if (par().flag == OpIIIFlag::LeftRight)
+    {        
+        Gamma::Algebra sigma_iX; 
+        Gamma::Algebra sigma_iY;
+        Gamma::Algebra sigma_iZ;
+        switch(par().index){
+            case 0:
+                sigma_iX = 0.*sigma_iX;
+                sigma_iY = Gamma::Algebra::SigmaXY;
+                sigma_iZ = Gamma::Algebra::SigmaXZ;
+                break;
+            case 1:
+                sigma_iX = Gamma::Algebra::MinusSigmaXY;
+                sigma_iY = 0.*sigma_iY;
+                sigma_iZ = Gamma::Algebra::SigmaYZ;
+                break;
+            case 2:
+                sigma_iX = Gamma::Algebra::MinusSigmaXZ;
+                sigma_iY = Gamma::Algebra::MinusSigmaYZ;
+                sigma_iZ = 0.*sigma_iZ;
+                break;
+            case 3:
+                sigma_iX = Gamma::Algebra::MinusSigmaXT;
+                sigma_iY = Gamma::Algebra::MinusSigmaYT;
+                sigma_iZ = Gamma::Algebra::MinusSigmaZT;
+                break;
+            default:
+                HADRONS_ERROR(Argument, "Index must be in {0, 1, 2, 3}."); 
+        }
+        PropagatorField insertion = 
+            (sigma_iX)*g5 * (GImpl::CovShiftForward(gauge_x,0,field) - GImpl::CovShiftBackward(gauge_x,0,field))
+          + (sigma_iY)*g5 * (GImpl::CovShiftForward(gauge_y,1,field) - GImpl::CovShiftBackward(gauge_y,1,field))
+          + (sigma_iZ)*g5 * (GImpl::CovShiftForward(gauge_z,2,field) - GImpl::CovShiftBackward(gauge_z,2,field));
+
+        out = insertion;
+    }
 }
 
 END_MODULE_NAMESPACE
