6ed157f2dc45814dc72fb0a7dafac06571254879
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2023-02-13 16:21:16 +0000

    Added source cast guesser

---
+++ Hadrons/Modules/MGuesser/DeflationSrcCast.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MGuesser/DeflationSrcCast.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MGuesser;
+
+template class Grid::Hadrons::MGuesser::TDeflationSrcCast<FIMPL,FIMPLF>;
