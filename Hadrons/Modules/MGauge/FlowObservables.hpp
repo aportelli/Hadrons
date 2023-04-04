89a011569e03c4d753aedf5c72991aea15905460
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-04-04 17:34:37 +0100

    feat: hdf5 output written for FlowObservables module.

--- Hadrons/Modules/MGauge/FlowObservables.hpp
+++ Hadrons/Modules/MGauge/FlowObservables.hpp
@@ -45,7 +45,8 @@ public:
     GRID_SERIALIZABLE_CLASS_MEMBERS(FlowObservablesPar,
                                     std::string, gauge,
                                     Real, epsilon,
-                                    unsigned int, Nstep);
+                                    unsigned int, Nstep,
+                                    std::string, output);
 };
 
 template <typename GImpl>
@@ -53,6 +54,13 @@ class TFlowObservables: public Module<FlowObservablesPar>
 {
 public:
     GAUGE_TYPE_ALIASES(GImpl,);
+    class FlowResult: Serializable
+    {
+    public:
+        GRID_SERIALIZABLE_CLASS_MEMBERS(FlowResult,
+                                        std::string, description,
+                                        std::vector<RealD>, data);
+    };
 public:
     // constructor
     TFlowObservables(const std::string name);
@@ -61,6 +69,7 @@ public:
     // dependency relation
     virtual std::vector<std::string> getInput(void);
     virtual std::vector<std::string> getOutput(void);
+    virtual std::vector<std::string> getOutputFiles(void);
     // setup
     virtual void setup(void);
     // execution
@@ -94,6 +103,14 @@ std::vector<std::string> TFlowObservables<GImpl>::getOutput(void)
     return out;
 }
 
+template <typename GImpl>
+std::vector<std::string> TFlowObservables<GImpl>::getOutputFiles(void)
+{
+    std::vector<std::string> output = {resultFilename(par().output)};
+
+    return output;
+}
+
 // setup ///////////////////////////////////////////////////////////////////////
 template <typename GImpl>
 void TFlowObservables<GImpl>::setup(void)
@@ -109,48 +126,41 @@ void TFlowObservables<GImpl>::execute(void)
     auto &U = envGet(GaugeField, par().gauge);
 
     envGetTmp(GaugeField, Usmear);
-
     Usmear = U;
-    std::vector<std::pair<RealD,RealD>> plaquette_density;
-    std::vector<std::pair<RealD,RealD>> clover_density;
-    std::vector<std::pair<RealD,RealD>> topq;
 
-    plaquette_density.resize(0);
-    clover_density.resize(0);
-    topq.resize(0);
+    std::vector<FlowResult> flow_result;
+    flow_result.resize(4);
+
+    flow_result[0].description = "Flow time";
+    flow_result[1].description = "Plaquette energy density";
+    flow_result[2].description = "Clover energy density";
+    flow_result[3].description = "Topological charge";
 
     WilsonFlow<PeriodicGimplR> wflow(par().epsilon, par().Nstep, 1);
     wflow.resetActions();
 
-    wflow.addMeasurement(1, [&wflow, &clover_density, &plaquette_density](int step, RealD t, const LatticeGaugeField &U){
-        RealD tmp_plaquette = wflow.energyDensityPlaquette(t,U);
-        LOG(Debug) << "Plaquette energy density for step " << step << ", t=" << t << ", E=" << tmp_plaquette << std::endl;
-        plaquette_density.push_back( {t, tmp_plaquette} );
+    wflow.addMeasurement(1, [&wflow, &flow_result](int step, RealD t, const LatticeGaugeField &U){
+        flow_result[0].data.push_back(t);
+        flow_result[1].data.push_back(wflow.energyDensityPlaquette(t,U));
+        flow_result[2].data.push_back(wflow.energyDensityCloverleaf(t,U));
+        flow_result[3].data.push_back(WilsonLoops<GImpl>::TopologicalCharge(U));
 
-        RealD tmp_clover = wflow.energyDensityCloverleaf(t,U);
-        LOG(Debug) << "Cloverleaf energy density for step " << step << ", t=" << t << ", E=" << tmp_clover << std::endl;
-        clover_density.push_back( {t, tmp_clover} );
-    });
-    wflow.addMeasurement(1, [&wflow, &topq](int step, RealD t, const LatticeGaugeField &U){
-        RealD tmp_topq = WilsonLoops<GImpl>::TopologicalCharge(U);
-        LOG(Debug) << "Topological charge for step " << step << ", t=" << t << ", Q=" << tmp_topq << std::endl;
-        topq.push_back({t, tmp_topq});
+        LOG(Debug) << "Step " << step << std::endl;
+        for (unsigned int i = 0; i < 4; ++i)
+        {
+            LOG(Debug) << std::setw(25) << flow_result[i].description << " " << flow_result[i].data.back() << std::endl;
+        }
+        LOG(Debug) << std::endl;
     });
 
     wflow.smear(Usmear, U);
 
-    Real qtop = WilsonLoops<GImpl>::TopologicalCharge(Usmear);
+    for (unsigned int i = 1; i < 4; ++i)
+    {
+        LOG(Message) << std::setw(25) << flow_result[i].description << " : " << flow_result[i].data.back() << std::endl;
+    }
 
-    LOG(Message) << "Old method Topological charge: " << qtop << std::endl;
-    LOG(Message) << "Topological charge " << topq.back().second << " at flow time " << topq.back().first << std::endl;
-
-    LOG(Message) << "Energy density:" << std::endl;
-    for (auto en: clover_density)
-        LOG(Message) << en << std::endl;
-
-    LOG(Message) << "Topological charge:" << std::endl;
-    for (auto en: topq)
-        LOG(Message) << en << std::endl;
+    saveResult(par().output, "FlowObservables", flow_result);
 }
 
 END_MODULE_NAMESPACE
