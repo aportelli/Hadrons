22ef62a2424074dbfef0747b7db531a33c347d54
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-04-04 16:48:06 +0100

    feat: rewrote module to work with dirichlet merge.

--- Hadrons/Modules/MGauge/FlowObservables.hpp
+++ Hadrons/Modules/MGauge/FlowObservables.hpp
@@ -45,7 +45,7 @@ public:
     GRID_SERIALIZABLE_CLASS_MEMBERS(FlowObservablesPar,
                                     std::string, gauge,
                                     Real, epsilon,
-                                    Real, maxTau);
+                                    unsigned int, Nstep);
 };
 
 template <typename GImpl>
@@ -110,13 +110,47 @@ void TFlowObservables<GImpl>::execute(void)
 
     envGetTmp(GaugeField, Usmear);
 
-    WilsonFlow<PeriodicGimplR> WF(0, par().epsilon, 1);
-    WF.smear_adaptive(Usmear, U, par().maxTau);
+    Usmear = U;
+    std::vector<std::pair<RealD,RealD>> plaquette_density;
+    std::vector<std::pair<RealD,RealD>> clover_density;
+    std::vector<std::pair<RealD,RealD>> topq;
+
+    plaquette_density.resize(0);
+    clover_density.resize(0);
+    topq.resize(0);
+
+    WilsonFlow<PeriodicGimplR> wflow(par().epsilon, par().Nstep, 1);
+    wflow.resetActions();
+
+    wflow.addMeasurement(1, [&wflow, &clover_density, &plaquette_density](int step, RealD t, const LatticeGaugeField &U){
+        RealD tmp_plaquette = wflow.energyDensityPlaquette(t,U);
+        LOG(Debug) << "Plaquette energy density for step " << step << ", t=" << t << ", E=" << tmp_plaquette << std::endl;
+        plaquette_density.push_back( {t, tmp_plaquette} );
+
+        RealD tmp_clover = wflow.energyDensityCloverleaf(t,U);
+        LOG(Debug) << "Cloverleaf energy density for step " << step << ", t=" << t << ", E=" << tmp_clover << std::endl;
+        clover_density.push_back( {t, tmp_clover} );
+    });
+    wflow.addMeasurement(1, [&wflow, &topq](int step, RealD t, const LatticeGaugeField &U){
+        RealD tmp_topq = WilsonLoops<GImpl>::TopologicalCharge(U);
+        LOG(Debug) << "Topological charge for step " << step << ", t=" << t << ", Q=" << tmp_topq << std::endl;
+        topq.push_back({t, tmp_topq});
+    });
+
+    wflow.smear(Usmear, U);
 
     Real qtop = WilsonLoops<GImpl>::TopologicalCharge(Usmear);
 
-    LOG(Message) << "Topological charge: " << qtop << std::endl;
+    LOG(Message) << "Old method Topological charge: " << qtop << std::endl;
+    LOG(Message) << "Topological charge " << topq.back().second << " at flow time " << topq.back().first << std::endl;
 
+    LOG(Message) << "Energy density:" << std::endl;
+    for (auto en: clover_density)
+        LOG(Message) << en << std::endl;
+
+    LOG(Message) << "Topological charge:" << std::endl;
+    for (auto en: topq)
+        LOG(Message) << en << std::endl;
 }
 
 END_MODULE_NAMESPACE
