ce9430daedf764d531bfa22f555bb2b810e6807a
Ryan Hill <rchrys.hill@gmail.com>
2024-01-17 13:47:01 +0000

    Add ExactQEDL module

---
+++ Hadrons/Modules/MGauge/ExactQedL.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MGauge/ExactQedL.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MGauge;
+
+template class HADRONS_NAMESPACE::MGauge::TExactQedL<vComplex>;
