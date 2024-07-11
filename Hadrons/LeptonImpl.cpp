9f5ed3e818d6293f84c950e135015062309dbaa8
Antonin Portelli <antonin.portelli@me.com>
2024-07-11 12:00:22 +0900

    fix for architectures using ASM Dslash

--- Hadrons/LeptonImpl.cpp
+++ Hadrons/LeptonImpl.cpp
@@ -30,16 +30,7 @@
 #include <Grid/qcd/action/fermion/implementation/CayleyFermion5Dcache.h>
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsImplementation.h>
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsHandImplementation.h>
-
-#ifndef AVX512
-#ifndef QPX
-#ifndef A64FX
-#ifndef A64FXFIXEDSIZE
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsAsmImplementation.h>
-#endif
-#endif
-#endif
-#endif
 
 NAMESPACE_BEGIN(Grid);
 
